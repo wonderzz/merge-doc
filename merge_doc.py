20003424e21436c581af2a24c91efdd718001946
wonderzz <wander_zz@163.com>
2019-08-20 21:07:52 +0800

    提交合并步骤

---
+++ merge_doc.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-：
+#    Author : wonder_zz
+#    Time   : 2019/8/20  20:41
+import sys
+import os
+import re
+import time
+import pickle
+import codecs
+import string
+import shutil
+from docx import Document
+from win32com import client as wc
+
+
+def ReSaveDoc(path, filename):
+    """
+    将doc转换docx
+    """
+    pass
+
+
+def ReSaveAllDoc(path):
+    """
+    保存全部文档
+    """
+    pass
+
+
+def combine_word_documents(path, files):
+    """
+    创建合并文件
+    """
+    pass
+
+
+def MergeDocx(path):
+    """
+    合并文件
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
