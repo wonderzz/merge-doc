f10d71c1d2fb8907d1de1d595231186535d66d5d
wonderzz <wonder_zz@163.com>
2019-08-22 00:24:22 +0800

    Today is a little busy,So a big problem is not finish. but i want show i did something.yeah! that's all.

--- merge_doc.py
+++ merge_doc.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-：
 #    Author : wonder_zz
 #    Time   : 2019/8/20  20:41
-import sys
 import os
-import re
+import sys
 import time
-import pickle
-import codecs
-import string
-import shutil
 from docx import Document
 from win32com import client as wc
 
@@ -17,29 +12,68 @@ def ReSaveDoc(path, filename):
     """
     将doc转换docx
     """
-    pass
+    time1 = time.time()
+    word = wc.Dispatch('Word.Application')
+    doc = word.Documents.Open(path + "\\" + filename)
+    filename = filename.replace("doc", "docx")
+    doc.SaveAs(path + "\\" + filename, 12, False, "",
+               True, "", False, False, False, False)
+    doc.Close()
+    word.Quit()
+    time2 = time.time()
+    print("success change file " + filename + " in " + str(time2 - time1))
 
 
 def ReSaveAllDoc(path):
     """
     保存全部文档
     """
-    pass
+    filelist = []
+    dirs = os.listdir()
+    for f in dirs:
+        filelist.append(str(f))
+    for file in filelist:
+        if file.find(".doc") != -1:
+            ReSaveDoc(path, file)
+            print(file)
+        else:
+            continue
 
 
 def combine_word_documents(path, files):
     """
     创建合并文件
     """
-    pass
+    merged_document = Document()
+    for index, file in enumerate(files):
+        sub_doc = Document(path + "\\" + file)
+
+        # Don't add a page break if you've reached the last file.
+        if index < len(files) - 1:
+            sub_doc.add_page_break()
+
+        for element in sub_doc.element.body:
+            merged_document.element.body.append(element)
+
+    merged_document.save(path + "\\" + '合并文件.docx')
 
 
 def MergeDocx(path):
     """
     合并文件
     """
-    pass
+    files = []
+    filelist = []
+    dirs = os.listdir()
+    for f in dirs:
+        filelist.append(str(f))
+    for file in filelist:
+        if (file.find(".docx") != -1) and (file.find("~$") == -1):
+            files.append(file)
+    combine_word_documents(path, files)
 
 
 if __name__ == '__main__':
-    pass
+    path = os.getcwd()
+    ReSaveAllDoc(path)
+    MergeDocx(path)
